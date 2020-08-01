7f7d85193bf0c0af26141df2c05877c4eadf7f5d
Cheshi Mantu <cheshi.mantu@protonmail.com>
2020-08-01 14:07:25 +0300

    looking for right selectors

--- src/test/java/tests/MailTestsUiApi.java
+++ src/test/java/tests/MailTestsUiApi.java
@@ -7,22 +7,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class MailTestsUiApi {
-@Test
-@Tag("ui_api_tests")
+
 @Feature("qa-guru course 1")
 @Story("UI and API tests")
+public class MailTestsUiApi extends TestBase {
+
+@Test
+@Tag("ui_api_tests")
 @DisplayName("Open yandex mail, authenticate, send email to myself")
-@Description
+@Description("attempt 1")
 void logInSendMailToMyself(){
     step("", ()->{
         open("https://mail.yandex.ru");
-
-//        open("https://mail.yandex.ru");
-
+        $(".HeadBanner-Button-Enter").shouldBe(visible).click();
+        $(".passp-form-field__label").should(exist);// this is label of log-in field saying "Введите логин, почту или телефон"
+        $("[data-t='field:login']").shouldBe(visible);
     });
 //    step("", ()->{
 //
@@ -41,4 +44,31 @@ void logInSendMailToMyself(){
 //    });
 
     }
+//@Test
+//@Tag("ui_api_tests")
+//@DisplayName("Open yandex mail, authenticate, send email to myself")
+//@Description("Attempt 2")
+//void logInSendMailToMyself1(){
+//    step("", ()->{
+//        open("https://mail.yandex.ru");
+//
+//
+//    });
+//    step("", ()->{
+//
+//    });
+//    step("", ()->{
+//
+//    });
+//    step("", ()->{
+//
+//    });
+//    step("", ()->{
+//
+//    });
+//    step("", ()->{
+//
+//    });
+//
+//    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,20 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
 public class TestBase {
+    @BeforeAll
+    static void setUpTest(){
+        Configuration.startMaximized = true;
+    }
+
+    @AfterEach
+    public void afterEach() {
+        closeWebDriver();
+    }
+
 }
